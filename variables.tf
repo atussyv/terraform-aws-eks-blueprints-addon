d40f55ec8d28c75be3330ce423c8db888440e6c6
Arnaud Tussy-Vassilieff <arnaud.tussy-vassilief@vev.com>
2025-04-22 14:25:46 +0200

    feat: sensitive value accepts ephemeral values as input

--- variables.tf
+++ variables.tf
@@ -234,6 +234,7 @@ variable "set_sensitive" {
   description = "Value block with custom sensitive values to be merged with the values yaml that won't be exposed in the plan's diff"
   type        = any
   default     = []
+  ephemeral   = true
 }
 
 variable "set_irsa_names" {
